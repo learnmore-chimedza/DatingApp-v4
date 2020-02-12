ad0e9834ae1ec6a7a0adcc50dbe7448b468a9c9e
Learnmore Chimedza <chimedzalearnmore@gmail.com>
2020-02-12 10:55:19 +0200

    End of Section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.loggin(this.model).subscribe(next => {
       console.log('logged in successfully');
     }, error => {
-      console.log('Failed to logg in');
+      console.log(error);
     });
   }
 
